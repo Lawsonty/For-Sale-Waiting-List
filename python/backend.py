d9b0d1be7fa1dd60769af542f1708170be4bc36a
Tyler Lawson <lawsonty@oregonstate.edu>
2018-08-21 15:00:22 -0700

    Can add and delete customers

--- python/backend.py
+++ python/backend.py
@@ -7,9 +7,9 @@ class DBM:
         c = self.conn.cursor()
         c.execute('''CREATE TABLE IF NOT EXISTS movies(id INTEGER PRIMARY KEY
                      AUTOINCREMENT, title TEXT)''')
-        c.execute('''CREATE TABLE IF NOT EXISTS  customers(id INTEGER
-                     , name TEXT NOT NULL, phone NOT NULL,
-                     PRIMARY KEY(name, phone))''')
+        c.execute('''CREATE TABLE IF NOT EXISTS  customers(id INTEGER PRIMARY
+                     KEY AUTOINCREMENT, name TEXT NOT NULL, phone TEXT NOT
+                     NULL, UNIQUE(name, phone))''')
         self.conn.commit()
 
     def addMovie(self, title):
@@ -27,13 +27,24 @@ class DBM:
         self.conn.commit()
 
     def addCustomer(self, name, number):
-        pass
+        c = self.conn.cursor()
+        c.execute('''INSERT INTO customers (name, phone)
+                     VALUES (?, ?)''', (name, number))
+        self.conn.commit()
+
+    def delCustomer(self, name, number):
+        c = self.conn.cursor()
+        c.execute('''DELETE FROM customers WHERE name=? AND phone=?''', (name, number))
+        self.conn.commit()
 
 
 if __name__ == '__main__':
     dbm = DBM()
     dbm.addMovie("Saw IV")
-    dbm.addMovie("Saw V")
     print(dbm.conn.cursor().execute("SELECT * FROM movies").fetchall())
     dbm.delMovie("Saw IV")
     print(dbm.conn.cursor().execute("SELECT * FROM movies").fetchall())
+    dbm.addCustomer("Johnny Franko", 5034051367)
+    print(dbm.conn.cursor().execute("SELECT * FROM customers").fetchall())
+    dbm.delCustomer("Johnny Franko", 5034051367)
+    print(dbm.conn.cursor().execute("SELECT * FROM customers").fetchall())
